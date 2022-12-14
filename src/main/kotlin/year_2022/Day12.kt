ca1377b512528405780c5c99c4d77259c89f4372
Rudiyanto <rynto14@gmail.com>
2022-12-14 18:27:51 +0700

    day 14 😭

--- src/main/kotlin/year_2022/Day12.kt
+++ src/main/kotlin/year_2022/Day12.kt
@@ -1,15 +1,15 @@
 package year_2022
 
-import util.Pos
+import util.Pos2
 import util.Runner
 import util.wrap
 import java.util.LinkedList
 import java.util.Queue
 
 fun main() {
-    fun <T> List<List<T>>.get(p: Pos): T = this[p.v][p.h]
-    fun <T> List<MutableList<T>>.set(p: Pos, data: T) {
-        this[p.v][p.h] = data
+    fun <T> List<List<T>>.get(p: Pos2): T = this[p.x][p.y]
+    fun <T> List<MutableList<T>>.set(p: Pos2, data: T) {
+        this[p.x][p.y] = data
     }
 
     fun canClimb(tmpCurrentHeight: Char, destHeight: Char): Boolean {
@@ -17,8 +17,8 @@ fun main() {
         return destHeight == 'S' || (destHeight in currentHeight - 1..currentHeight) || currentHeight <= destHeight
     }
 
-    fun traverse(map: List<List<Char>>, start: Pos, target: Char): Int {
-        val q: Queue<Triple<Pos, Char, Int>> = LinkedList()
+    fun traverse(map: List<List<Char>>, start: Pos2, target: Char): Int {
+        val q: Queue<Triple<Pos2, Char, Int>> = LinkedList()
         val vLen = map.size
         val hLen = map.first().size
         val visited = List(vLen) { MutableList(hLen) { false } }
@@ -28,7 +28,7 @@ fun main() {
         while(q.isNotEmpty()) {
             val (pos, lastChar, score) = q.remove()
 
-            if (pos.v !in 0 until vLen || pos.h !in 0 until hLen) continue
+            if (pos.x !in 0 until vLen || pos.y !in 0 until hLen) continue
             if (visited.get(pos)) continue
             if (map.get(pos) != 'E' && !canClimb(lastChar, map.get(pos))) continue
             if (map.get(pos) == target) return score
@@ -36,24 +36,24 @@ fun main() {
             visited.set(pos, true)
 
             wrap(map.get(pos), score + 1) { a, b ->
-                q.add(Triple(pos.left(), a, b))
-                q.add(Triple(pos.top(), a, b))
-                q.add(Triple(pos.right(), a, b))
-                q.add(Triple(pos.bottom(), a, b))
+                q.add(Triple(pos.getLeft(), a, b))
+                q.add(Triple(pos.getTop(), a, b))
+                q.add(Triple(pos.getRight(), a, b))
+                q.add(Triple(pos.getBottom(), a, b))
             }
         }
 
         return -1
     }
 
-    fun prepareMap(input: String):  Pair<List<List<Char>>, Pos> {
-        var end = Pos.ZERO
+    fun prepareMap(input: String):  Pair<List<List<Char>>, Pos2> {
+        var end = Pos2.ZERO
         val map = input.split("\n").map {
             it.toList()
         }
         for (i in map.indices) {
-            end = Pos(i, map[i].indexOf('E'))
-            if (end.h != -1) break
+            end = Pos2(i, map[i].indexOf('E'))
+            if (end.y != -1) break
         }
 
         return Pair(map, end)
