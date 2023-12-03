18c9992a1c26b7c4716855ccc6f4fb2c325a61c6
Rudiyanto <rudi.rudiyanto@agoda.com>
2023-12-03 14:01:29 +0700

    day 2

--- advent-of-code-2023/main.gd
+++ advent-of-code-2023/main.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var util = preload("res://src/day_01.gd").new()
+	var util = preload("res://src/day_02.gd").new()
 	pass
 	
 
