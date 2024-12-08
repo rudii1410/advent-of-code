c1831971c80eb7e083403f0442b934389b422ef1
Rudiyanto <rynto14@gmail.com>
2024-12-08 14:40:52 +0700

    day 7

--- advent-of-code-2024/src/main/kotlin/problem/DayN.kt
+++ advent-of-code-2024/src/main/kotlin/problem/DayN.kt
@@ -2,20 +2,20 @@ package problem
 
 import util.Runner
 
-private fun part01(input: List<String>): String {
-    return ""
+private fun part01(input: List<String>): Int {
+    return 0
 }
 
-private fun part02(input: List<String>): String {
-    return ""
+private fun part02(input: List<String>): Int {
+    return 0
 }
 
 fun main() {
     Runner.get(::part01)
-        .test("", "")
-        .run("")
+        .test("dayN_test.in", 0)
+        .run("dayN.in")
 
     Runner.get(::part02)
-        .test("", "")
-        .run("")
+        .test("dayN_test.in", 0)
+        .run("dayN.in")
 }
