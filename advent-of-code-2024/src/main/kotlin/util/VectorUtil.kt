ca05757a564c5035a230b34159743ca5225d6e05
Rudiyanto <rudi.rudiyanto@agoda.com>
2024-12-16 17:30:49 +0700

    day 13

--- advent-of-code-2024/src/main/kotlin/util/VectorUtil.kt
+++ advent-of-code-2024/src/main/kotlin/util/VectorUtil.kt
@@ -29,3 +29,5 @@ fun Vector2.withinBound(size: Int): Boolean {
 fun Vector2.withinBound(xSize: Int, ySize: Int): Boolean {
     return x >= 0 && y >= 0 && x < xSize && y < ySize
 }
+
+fun Pair<Int, Int>.toVec2() = Vector2(x = first, y = second)
